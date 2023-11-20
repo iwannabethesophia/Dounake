366190ecb144a3e404e408d3555e90e741d4033e
iwannabethesophia <pydevmonkey4002@gmail.com>
2023-11-20 19:12:16 +0700

    cập nhật trạng thái táo vàng cho fruit

--- src/snake/fruit.py
+++ src/snake/fruit.py
@@ -3,8 +3,12 @@ from .point import Point
 import random
 
 class Fruit(Point):
+    # variable for handling the golden apple
+    is_golden = False
+
     def __init__(self, x = None, y = None):
         super().__init__(random.randint(1, 30), random.randint(2, 30))
+        self.is_golden = False
 
     def nextFruitPosition(self):
         self.x = random.randint(1, 30)
